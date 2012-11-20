b73618cf010fc83feefc3f8cbe96e675f5a01eff
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-11-20 22:10:44 +0100

    grafico temp

--- temperaturas/management/commands/temperaturas.py
+++ temperaturas/management/commands/temperaturas.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import User
 import datetime, qsstats
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
@@ -8,6 +7,7 @@ from django.utils import timezone
 import serial
 import time
 from control.myserial import myserial
+from django.contrib.auth import User
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
