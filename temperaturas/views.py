76e2cc442246ed21dc2952c8842a84eb2ea5dcc0
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-11-20 22:06:57 +0100

    vista grafico 1

--- temperaturas/views.py
+++ temperaturas/views.py
@@ -1,4 +1,24 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import User
+import datetime
+import qsstats
+
+def estadisticas(request):
+
+    GOOGLE_API_KEY = 'clave'
+
+    qs = User.objects.all()
+    qss = qsstats.QuerySetStats(qs, 'temperaturas_temperatura')
+
+    hoy = datetime.date.today()
+    hace_2_semanas = hoy - datetime.timedelta(weeks=2)
+
+    users_stats = qss.time_series(hace_2_semanas, hoy)
+
+    return render_to_response('estadisticas.html', locals(), context_instance=RequestContext(request))
+
+
 
 
   
