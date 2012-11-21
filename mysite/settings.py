cff9fe5ac58a68798e9e73ad2905c0282262c69d
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-11-21 01:42:47 +0100

    temsudo temper

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'moisture',
     'control',
     'qsstats',
+    'chartit',
 )
 
 # A sample logging configuration. The only tangible logging
--- temperaturas/views.py
+++ temperaturas/views.py
@@ -1,4 +1,36 @@
+from chartit import DataPool, Chart
 
+def weather_chart_view(request):
+    #Step 1: Create a DataPool with the data we want to retrieve.
+    tempdata = \
+        DataPool(
+           series=
+            [{'options': {
+               'source': Temperatura.objects.all()},
+              'terms': [
+                'temperatura',
+                'pub_date']}
+             ])
+
+    #Step 2: Create the Chart object
+    cht = Chart(tempdata,
+            series_options =
+              [{'options':{
+                  'type': 'line',
+                  'stacking': False},
+                'terms':{
+                  'temperatura': [
+                    'pub_date']
+                  }}],
+            chart_options =
+              {'title': {
+                   'text': 'Temperatura Diaria'},
+               'xAxis': {
+                    'title': {
+                       'text': 'Temperatura'}}})
+
+    #Step 3: Send the chart object to the template.
+    return render_to_response({'weatherchart': cht})
 
 
 
