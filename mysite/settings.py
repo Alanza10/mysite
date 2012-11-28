67bdb59c1a52a416d48ed5b93a0b6adeaba2f227
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-11-28 16:49:47 +0100

    statci 6

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,11 +59,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = '/var/django/mysite/static/'
+STATIC_ROOT = '/var/django/mysite/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
