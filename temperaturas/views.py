ef834c0f868bab09e655d2a598d68c60ed6f46d3
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-12-01 14:10:45 +0100

    cambios temp2

--- temperaturas/views.py
+++ temperaturas/views.py
@@ -5,16 +5,44 @@ from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
 from GChartWrapper import *
 from GChartWrapper.constants import _print
+from temperaturas.models import Temperatura
 
 def results(request):
-    # Add red line 6 thick
-    # with 5 line segments with 2 blank segments
-    G = Line( ['hX1xPj'] )
-    G.axes('xy')
-    G.axes.label(0, 'Mar', 'Apr', 'May', 'June', 'July')
-    G.axes.label(1, None, '50+Kb')        
-    G.color('red')
-    G.line(6,5,2)
+    
+    temps=Temperatura.objects.order_by('-pub_date')[:10]
+ 
+
+    data1 = [0, 13, 28, 42, 56, 71, 84, 100]
+    
+    data2 =  [25, 24, 25, 23.5, 25, 24, 24, 24]
+    
+    # multiple axis with label positions specified
+    # values between 0 and 100 - use text encoding
+    data = [data1, 
+           data2]
+    
+    # positions between 0 and 100
+    axis = [ [0, 13, 28, 42, 56, 71, 84, 100],
+             ['1', '2', '3', '4', '5', '6', '7', '8'] ]
+    
+    # don't do integer arithmetic
+    min_value = float(min(data[1]))
+    max_value = float(max(data[1]))
+    last_value = float(data[1][-1])
+    
+    G = LineXY(data, encoding='text')
+    G.color('76A4FB')
+    G.marker('o', '0077CC',0,-1,5)
+    G.marker('r', 'E6F2FA',0,(min_value/max_value),1.0) # 0 to 1.0
+    G.axes("xyr")    
+    G.axes.label(0, *axis[1])
+    G.axes.position(0, *axis[0])
+    G.axes.label(1, 15, 20, 25, 35, 35, 40, 45, 50)    
+    G.axes.position(1, 0, 14, 28, 42, 56, 70, 84, 98) # 0 to 100
+    G.axes.label(2, '%d'%last_value)
+    G.axes.position(2, int(100*last_value/max_value)) # 0 to 100
+
+    #volcar grafico en payload
     chart_url=G
     payload = {'chart_url':G}
     return render_to_response('temp/estadisticas.html', payload)
