fb23b3bc09651aae33cc83cd2a2b6085d15e9185
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-12-02 17:30:59 +0100

    grafico temperaturas

--- temperaturas/views.py
+++ temperaturas/views.py
@@ -12,7 +12,7 @@ def results(request):
     temps=Temperatura.objects.order_by('-pub_date')[:10]
  
 
-    data1 = [0, 13, 28, 42, 56, 71, 84, 100]
+    data1 = [2, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65, 69, 73, 79, 83, 87, 91, 95 ]
     
     data2 =  [25, 24, 25, 23.5, 25, 24, 24, 24.5]
     
@@ -22,8 +22,9 @@ def results(request):
            data2]
     
     # positions between 0 and 100
-    axis = [ [0, 13, 28, 42, 56, 71, 84, 100],
-             ['1', '2', '3', '4', '5', '6', '7', '8'] ]
+    axis = [ [2, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65, 69, 73, 79, 83, 87, 91, 95 ],
+             ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+            '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00'] ]
  
 
         
@@ -34,13 +35,15 @@ def results(request):
     
     G = LineXY(data, encoding='text')
     G.color('76A4FB')
-    G.size(500, 240)
+    G.size(600, 240)
+    G.scale('0', '100', '10', '45')
     G.marker('o', '0077CC',0,-1,5)
-    G.marker('r', '7fffd4',0,0.25,0.28) # 0 to 1.0
+    G.marker('r', '7fffd4',0,0.35,0.60) # 0 to 1.0
     G.axes("xyr")    
     G.axes.label(0, *axis[1])
     G.axes.position(0, *axis[0])
-    G.axes.range(1, 10, 100)  
+    G.axes.range(0, 0, 100)
+    G.axes.range(1, 10, 45)  
     G.axes.label(1, 10, 15, 20, 25, 30, 35, 40, 45)    
     G.axes.position(1, 10, 15, 20, 25, 30, 35, 40, 45) # 0 to 100
     G.axes.label(2, '%d'%last_value)
