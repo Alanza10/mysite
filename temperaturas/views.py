64aa61cc6d0e3d15afed42b4c0e68ef9ad28107d
gelo <gelo@gelo-PC.alanza10.dyndns.org>
2012-12-02 15:40:55 +0100

    temps view

--- temperaturas/views.py
+++ temperaturas/views.py
@@ -14,7 +14,7 @@ def results(request):
 
     data1 = [0, 13, 28, 42, 56, 71, 84, 100]
     
-    data2 =  [25, 24, 25, 23.5, 25, 24, 24, 24]
+    data2 =  [25, 24, 25, 23.5, 25, 24, 24, 24.5]
     
     # multiple axis with label positions specified
     # values between 0 and 100 - use text encoding
@@ -24,7 +24,9 @@ def results(request):
     # positions between 0 and 100
     axis = [ [0, 13, 28, 42, 56, 71, 84, 100],
              ['1', '2', '3', '4', '5', '6', '7', '8'] ]
-    
+ 
+
+        
     # don't do integer arithmetic
     min_value = float(min(data[1]))
     max_value = float(max(data[1]))
@@ -32,13 +34,15 @@ def results(request):
     
     G = LineXY(data, encoding='text')
     G.color('76A4FB')
+    G.size(500, 240)
     G.marker('o', '0077CC',0,-1,5)
-    G.marker('r', 'E6F2FA',0,(min_value/max_value),1.0) # 0 to 1.0
+    G.marker('r', '7fffd4',0,0.25,0.28) # 0 to 1.0
     G.axes("xyr")    
     G.axes.label(0, *axis[1])
     G.axes.position(0, *axis[0])
-    G.axes.label(1, 15, 20, 25, 35, 35, 40, 45, 50)    
-    G.axes.position(1, 0, 14, 28, 42, 56, 70, 84, 98) # 0 to 100
+    G.axes.range(1, 10, 100)  
+    G.axes.label(1, 10, 15, 20, 25, 30, 35, 40, 45)    
+    G.axes.position(1, 10, 15, 20, 25, 30, 35, 40, 45) # 0 to 100
     G.axes.label(2, '%d'%last_value)
     G.axes.position(2, int(100*last_value/max_value)) # 0 to 100
 
